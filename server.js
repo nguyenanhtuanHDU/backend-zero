58b1735cb37117d1384aae7246a21e7f200e0fce
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2022-12-21 06:03:45 +0700

    #22. Test method get of express

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/test', (req, res) => {
+  res.send('<h1>test title!</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
