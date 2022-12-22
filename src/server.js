4ae790ba458f4dc412627e306140eaa3bdb6831f
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2022-12-23 05:35:51 +0700

    #22. Mô hình MVC

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs')
 
 // config static file
 // app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public'))) // cấu hình static file tìm trong thư mục puclic
 
 // khai báo route
 app.get('/test', (req, res) => {
