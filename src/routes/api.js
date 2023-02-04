3b20d59c6848390c0625a69e795a8bcc9c50766f
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-02-04 15:33:25 +0700

    #94. Req.query - #95. Req.params

--- src/routes/api.js
+++ src/routes/api.js
@@ -34,4 +34,17 @@ router.put("/customer", putUpdateCustomer);
 router.delete("/customer", deleteCustomer);
 router.delete("/customer-list", deleteCustomerList);
 
+// req.query: dùng cả khi có ít dữ liệu và khi có nhiều dữ liệu
+router.get("/info", (req, res) => {
+  console.log(">>> check req.query: ", req.query);
+  res.send("req.query");
+});
+
+// req.params: chỉ dùng khi ít dữ liệu
+router.get("/info/:name/:address", (req, res) => {
+  console.log(">>> check req.params: ", req.params);
+  res.status(200).json({
+    data: req.params,
+  });
+});
 module.exports = router;
